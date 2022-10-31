708db4893dbaa3e2cd7b4195f9e497aaf34dccd6
vijaykumardumpeti <115571763+vijaykumardumpeti@users.noreply.github.com>
2022-10-31 13:23:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,37 +29,34 @@ intializeDBAndServer();
 //register API-1
 
 app.post("/register", async (request, response) => {
-  try {
-    let { username, name, password, gender, location } = request.body;
-    //password lessthan
-    let getUserDetails = `SELECT * FROM user WHERE username = ${username};`;
-    let dbUser = await db.get(getUserDetails);
-    let hashedPassword = await bcrypt.hash(password, 10);
-    let passwordLength = password.length();
-    if (dbUser === undefined) {
-      let createUserIntoDBQuery = `
-            INSERT INTO 
-                user (username, name, password, gender, location)
-                VALUES (
-                    '${username}',
-                    '${name}',
-                    '${hashedPassword}',
-                    '${gender}',
-                    '${location}',
-                )`;
-      let dbUser = await db.run(createUserIntoDBQuery);
-      let userId = dbUser.lastID;
-      response.status(200);
-      response.send("User created successfully");
-    } else if (passwordLength < 5) {
-      response.status(400);
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = SELECT * FROM user WHERE username = '${username}';
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) { /*Check the Usage of if -else conditions*/
+    if (request.body.password.length < 5) {
+      response.status("400");
       response.send("Password is too short");
-    } else if (dbUser !== undefined) {
-      response.status(400);
-      response.send("User already exists");
+    } else {
+      const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+      const dbResponse = await db.run(createUserQuery);
+      const newUserId = dbResponse.lastID;
+      response.status("200");
+      response.send(User created successfully);
     }
-  } catch (e) {
-    console.log(`DBError: ${e.message}`);
+  } else {
+    response.status("400");
+    response.send("User already exists");
   }
 });
 
